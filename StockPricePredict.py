de296bbc7dbe7c7ab289b4fdaf3bb0a31d08c1ad
BB-Chan <139550088+BB-Chan@users.noreply.github.com>
2024-10-27 12:38:17 +0800

    Update StockPricePredict.py

--- StockPricePredict.py
+++ StockPricePredict.py
@@ -51,7 +51,7 @@ if not button:
 stock = yf.download(code, start, end)
 stock.to_csv(code + '.csv')
 df = pd.read_csv(code + '.csv')
-df.dropna(thresh=0:1)
+df.dropna(thresh=1)
 st.header(code)
 st.subheader('Stock Data')
 st.dataframe(df)
