2cee09e537453ae641469387bddd2775772f37d8
BB-Chan <139550088+BB-Chan@users.noreply.github.com>
2024-11-07 14:53:02 +0800

    Update StockPricePredict.py

--- StockPricePredict.py
+++ StockPricePredict.py
@@ -89,7 +89,7 @@ df['MACD'] = 2 * (df['DIF'] - df['DEA'])
 st.dataframe(df)
 
 # Calculate Directional Movement Index
-def get_adx(df1, lookback):
+def get_adx(df1):
     plus_dm = df1['High'].diff()
     minus_dm = df1['Low'].diff()
     plus_dm[plus_dm < 0] = 0
@@ -99,15 +99,15 @@ def get_adx(df1, lookback):
     tr3 = pd.DataFrame(abs(df1['Low'] - df1['Close'].shift(1)))
     frames = [tr1, tr2, tr3]
     tr = pd.concat(frames, axis=1, join='inner').max(axis=1)
-    atr = tr.rolling(lookback).mean()
-    df1['Plus_di'] = 100 * (plus_dm.ewm(alpha=1 / lookback).mean() / atr)
-    df1['Minus_di'] = abs(100 * (minus_dm.ewm(alpha=1 / lookback).mean() / atr))
+    atr = tr.rolling(14).mean()
+    df1['Plus_di'] = 100 * (plus_dm.ewm(alpha=1 / 14).mean() / atr)
+    df1['Minus_di'] = abs(100 * (minus_dm.ewm(alpha=1 / 14).mean() / atr))
     dx = (abs(df1['Plus_di'] - df1['Minus_di']) / abs(df1['Plus_di'] + df1['Minus_di'])) * 100
-    adx = ((dx.shift(1) * (lookback - 1)) + dx) / lookback
-    df1['ADX'] = adx.ewm(alpha=1 / lookback).mean()
+    adx = ((dx.shift(1) * (14 - 1)) + dx) / lookback
+    df1['ADX'] = adx.ewm(alpha=1 / 14).mean()
     return df1
-lookback = 14
-df = get_adx(df, lookback)
+
+df = get_adx(df)
 st.dataframe(df)
 
 # Calculate KDJ
