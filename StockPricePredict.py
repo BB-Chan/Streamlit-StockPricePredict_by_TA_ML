2c9467bbfdb8633a7423c18d012a73359375a62b
BB-Chan <139550088+BB-Chan@users.noreply.github.com>
2024-11-07 14:24:57 +0800

    Update StockPricePredict.py

--- StockPricePredict.py
+++ StockPricePredict.py
@@ -87,28 +87,26 @@ df['DIF'] = df['EMA1'] - df['EMA2']
 df['DEA'] = df['DIF'].ewm(span=9, adjust=False, min_periods=9).mean()
 df['MACD'] = 2 * (df['DIF'] - df['DEA'])
 
-st.write(df.info())
 # Calculate Directional Movement Index
-def get_adx(high, low, close, lookback):
-    plus_dm = high.diff()
-    minus_dm = low.diff()
+def get_adx(df1, lookback):
+    plus_dm = df1['High'].diff()
+    minus_dm = df1['Low'].diff()
     plus_dm[plus_dm < 0] = 0
     minus_dm[minus_dm > 0] = 0
-    tr1 = pd.DataFrame(high - low)
-    tr2 = pd.DataFrame(abs(high - close.shift(1)))
-    tr3 = pd.DataFrame(abs(low - close.shift(1)))
+    tr1 = pd.DataFrame(df1['High'] - df1['Low'])
+    tr2 = pd.DataFrame(abs(df1['High'] - df1['Close'].shift(1)))
+    tr3 = pd.DataFrame(abs(df1['Low'] - df1['Close'].shift(1)))
     frames = [tr1, tr2, tr3]
     tr = pd.concat(frames, axis=1, join='inner').max(axis=1)
     atr = tr.rolling(lookback).mean()
-    plus_di = 100 * (plus_dm.ewm(alpha=1 / lookback).mean() / atr)
-    minus_di = abs(100 * (minus_dm.ewm(alpha=1 / lookback).mean() / atr))
-    dx = (abs(plus_di - minus_di) / abs(plus_di + minus_di)) * 100
+    df1['Plus_di'] = 100 * (plus_dm.ewm(alpha=1 / lookback).mean() / atr)
+    df1['Minus_di'] = abs(100 * (minus_dm.ewm(alpha=1 / lookback).mean() / atr))
+    dx = (abs(df1['Plus_di'] - df1['Minus_di']) / abs(df1['Plus_di'] + df1['Minus_di'])) * 100
     adx = ((dx.shift(1) * (lookback - 1)) + dx) / lookback
-    adx_smooth = adx.ewm(alpha=1 / lookback).mean()
-    return plus_di, minus_di, adx_smooth
-df['Plus_di'] = get_adx(df['High'], df['Low'], df['Close'], 14)[0]
-df['Minus_di'] = get_adx(df['High'], df['Low'], df['Close'], 14)[1]
-df['ADX'] = get_adx(df['High'], df['Low'], df['Close'], 14)[2]
+    df1['ADX'] = adx.ewm(alpha=1 / lookback).mean()
+    return df1
+df = get_adx(df, 14)
+st.dataframe(df)
 
 # Calculate KDJ
 def calKDJ(df2):
